2a6317869ecd9f53bf70e898ea38bae8d550e465
Khaled Khalifa <khaledmedhat64@gmail.com>
2022-05-05 22:53:07 +0200

    Replaced the old OpenLibrary API with the new Google Books API

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,74 @@
+let cards = document.querySelector(".cards");
+
+let item,title,author,publisher,bookLink,bookImg;
+let item2,title2,author2,publisher2,bookLink2,bookImg2;
+
+let bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+let placeHldr = "https://fakeimg.pl/320x500/";
+let searchData;
+
 document.querySelector("#searchForm").addEventListener("submit",function(e){
-    e.preventDefault();
-    document.querySelector("#output").innerHTML ="";
-    fetch("http://openlibrary.org/search.json?q="+document.querySelector("#search_query").value)
-    .then(a=>a.json())
-    .then(response => {
-        for(let i = 0 ;i < 10; i++){
-            document.querySelector("#output").innerHTML += "<h2>" + response.docs[i].title+ "</h2>" + response.docs[i].author_name[0]+"<br><img src = 'http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>";
+  e.preventDefault();
+  cards.innerHTML = "";
+  searchData = document.querySelector("#search_query").value;
+  if(searchData ==="" || searchData === null) {      
+  }
+  else{
+    $.ajax({
+        url: bookUrl + searchData,
+        dataType: "json",
+        success: function(res){
+          console.log(res);
+          if(res.totalItem === 0){
+            alert("No results found.");
+          }
+          else{
+            displayResults(res);
+          }
+        },
+        error: function(){
+          alert("Something went wrong.");
         }
     })
+  }
+  document.querySelector("#search_query").value="";
+  
 })
+
+function displayResults(res){
+  for(var i = 0 ; i < res.items.length;i++){
+
+    item = res.items[i];
+    title = item.volumeInfo.title;
+    author = item.volumeInfo.author;
+    publisher = item.volumeInfo.publisher;
+    bookLink =  item.volumeInfo.previewLink;
+    bookImg = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr;
+
+    
+    cards.innerHTML +=formatOutput(bookImg, title, author, publisher, bookLink);
+    console.log(cards);
+  }
+}
+
+function formatOutput(bookImg, title, author, publisher, bookLink) {
+  // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
+/*   var viewUrl = 'book.html?isbn='+bookIsbn; //constructing link for bookviewer
+ */  var htmlCard = `<div class="card">
+  <img src="${bookImg}" class="card__image">
+  <div class="card__content">
+          ${title}
+  </div>
+  <div class="card__info">
+
+      <a href="${bookLink}" target="__blank" class="card__link">Details</a>
+  </div>
+  
+</div>
+  `
+  return htmlCard;
+}
+
 
 ////////////////////////////////////////////////////////////////////////
 window.onscroll = function() {myFunction()};
