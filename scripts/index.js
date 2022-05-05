288533bcf936b8787ae8c2890b62240119f9c6f3
Khaled Khalifa <khaledmedhat64@gmail.com>
2022-05-05 11:59:23 +0200

    Adjusted the API calls and some CSS properties

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,21 @@ document.querySelector("#searchForm").addEventListener("submit",function(e){
         }
     })
 })
+
+////////////////////////////////////////////////////////////////////////
+window.onscroll = function() {myFunction()};
+// Get the navbar
+var searchbar = document.querySelector(".searchFormClass");
+
+// Get the offset position of the navbar
+var sticky = searchbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    searchbar.classList.add("sticky");
+  } else {
+    searchbar.classList.remove("sticky");
+  }
+}
+////////////////////////////////////////////////////////////////////////
