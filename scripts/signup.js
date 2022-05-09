bcde617428cabc3137f7b5a4c2c32873ba86faf4
Mohamed Magdy <magdim200@gmail.com>
2022-05-09 17:27:03 +0200

    linked signup page to login

--- scripts/signup.js
+++ scripts/signup.js
@@ -3,5 +3,5 @@ const reg = document.querySelector(".form-btn");
 reg.addEventListener("click",function(e){
 
     e.preventDefault();
-    location.href="../html/index.html";
+    location.href="../html/login.html";
 })
